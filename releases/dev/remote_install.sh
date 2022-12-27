f409fb4132e44a2e60e02ff55e0f86a33cb3d699
pa3gsb <pa3gsb@gmail.com>
2022-12-27 14:08:38 +0100

    versie update webservice

--- releases/dev/remote_install.sh
+++ releases/dev/remote_install.sh
@@ -13,28 +13,28 @@ function install_dependency {
 install_dependency git
 install_dependency default-jdk
 
-if [ -d "/home/pi/server/apache-tomee-webprofile-8.0.10" ]; then
+if [ -d "/home/pi/server/apache-tomee-webprofile-8.0.13" ]; then
 	echo ""
 	echo "Stop webserver and uninstall remote app (trying to stop can show errors)"
 	echo ""
-	sudo /home/pi/server/apache-tomee-webprofile-8.0.10/bin/catalina.sh stop
-	sudo rm /home/pi/server/apache-tomee-webprofile-8.0.10/webapps/remote.war
-	sudo rm -r /home/pi/server/apache-tomee-webprofile-8.0.10/webapps/remote
+	sudo /home/pi/server/apache-tomee-webprofile-8.0.13/bin/catalina.sh stop
+	sudo rm /home/pi/server/apache-tomee-webprofile-8.0.13/webapps/remote.war
+	sudo rm -r /home/pi/server/apache-tomee-webprofile-8.0.13/webapps/remote
 fi
 if [ ! -d "/home/pi/server" ]; then
 	sudo mkdir /home/pi/server
 fi
 
 
-if [ ! -d "/home/pi/server/apache-tomee-webprofile-8.0.10" ]; then
+if [ ! -d "/home/pi/server/apache-tomee-webprofile-8.0.13" ]; then
 	echo ""
 	echo "Webserver not installed; download and install java webserver"
 	echo ""
-	#sudo wget https://dlcdn.apache.org/tomee/tomee-8.0.10/apache-tomee-8.0.10-webprofile.tar.gz   
-	#sudo tar -xzvf apache-tomee-8.0.10-webprofile.tar.gz  -C /home/pi/server
-	sudo wget https://dlcdn.apache.org/tomee/tomee-8.0.10/apache-tomee-8.0.10-webprofile.zip 
-	unzip apache-tomee-8.0.10-webprofile.zip -d /home/pi/server
-	sudo chmod +x /home/pi/server/apache-tomee-webprofile-8.0.10/bin/catalina.sh
+	#sudo wget https://dlcdn.apache.org/tomee/tomee-8.0.13/apache-tomee-8.0.13-webprofile.tar.gz   
+	#sudo tar -xzvf apache-tomee-8.0.13-webprofile.tar.gz  -C /home/pi/server
+	sudo wget https://dlcdn.apache.org/tomee/tomee-8.0.13/apache-tomee-8.0.13-webprofile.zip 
+	unzip apache-tomee-8.0.13-webprofile.zip -d /home/pi/server
+	sudo chmod +x /home/pi/server/apache-tomee-webprofile-8.0.13/bin/catalina.sh
 fi
 
 
@@ -56,10 +56,10 @@ cd Radioberry-2.x-Remote/releases/dev
 
 pwd
 
-sudo cp c866b0e691eb97aa8fa46c4f8d281ade.pfx /home/pi/server/apache-tomee-webprofile-8.0.10/conf
-sudo cp server.xml /home/pi/server/apache-tomee-webprofile-8.0.10/conf
+sudo cp c866b0e691eb97aa8fa46c4f8d281ade.pfx /home/pi/server/apache-tomee-webprofile-8.0.13/conf
+sudo cp server.xml /home/pi/server/apache-tomee-webprofile-8.0.13/conf
 
-sudo cp remote.war /home/pi/server/apache-tomee-webprofile-8.0.10/webapps
+sudo cp remote.war /home/pi/server/apache-tomee-webprofile-8.0.13/webapps
 
 echo ""
 echo "Server setup finished. Now start the server; use start_remote.sh (switch to su user)"
